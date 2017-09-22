49c6238563f38534e1dfb40d2a94c1fd61aca283
qinzhig <qinzhiguohit@gmail.com>
2017-09-22 18:21:36 +0800

    Version2

--- post_watch.js
+++ post_watch.js
@@ -8,6 +8,11 @@ var config = {
 
 var pool = new pg.Pool(config);
 
+var request = require('request');
+
+var site_root = 'http://192.168.1.144:3000';
+// var testj = { reference_id:33, name:'AK_BP', data:666};
+
 pool.connect(function(err,client,done){
 	if(err){
 		return console.error('DB Connection Error!',err);
@@ -17,6 +22,27 @@ pool.connect(function(err,client,done){
 	
 	client.on('notification', function(msg) {
     		console.log(msg);
+		var var1 = JSON.parse(msg.payload);
+		console.log(var1);	
+		var var2 = JSON.stringify(var1);
+		console.log(var2);
+
+		request({
+			url: site_root + '/categories',
+        		method: 'POST',
+			json: true, 
+			body: var2,
+			headers: {
+                		"content-type": "application/json;charset=utf-8",
+            		}
+		},
+        	function(err, res, body) {
+			if(!err){
+                		console.log(res);
+        		}
+			console.log(body);
+		
+		});
 	});
 	
 	client.query(watch_query, function(err,result){
